e65f99ddede720d55e23c91fe63cdbed93d82c3b
jackhopper <76411205+jackhopper@users.noreply.github.com>
2022-11-19 10:37:48 -0600

    Adding weekly files to repo 	modified:   Data Bowl 23 start.R
    "Initial files"

--- Data Bowl 23 start.R
+++ Data Bowl 23 start.R
@@ -1,22 +1,3 @@
-setwd("C:/Users/jackh/OneDrive/Documents/R Analytics/Data Cup 2022")
-#Data glossary here: https://www.kaggle.com/competitions/nfl-big-data-bowl-2023/data
-
-library(tidyverse)
-
-#####READ DATA
-games <- read_csv("games.csv")
-pffScoutingData <- read_csv("pffScoutingData.csv")
-players <- read_csv("players.csv")
-plays <- read_csv("plays.csv")
-week1 <- read_csv("week1.csv")
-week2 <- read_csv("week2.csv")
-week3 <- read_csv("week3.csv")
-week4 <- read_csv("week4.csv")
-week5 <- read_csv("week5.csv")
-week6 <- read_csv("week6.csv")
-week7 <- read_csv("week7.csv")
-week8 <- read_csv("week8.csv")
-
-all_weeks <- bind_rows(week1, week2, week3, week4, week5, week6, week7, week8)
-
-all_weeks$eventId <- paste(all_weeks$gameId, all_weeks$playId)
+version https://git-lfs.github.com/spec/v1
+oid sha256:847f5866c6d6126a15c82a477a878041dc2c8f7da06c57a468da7cff85b8c164
+size 755
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# data-bowl-22
-Kaggle Data Bowl 2022 Competition
+version https://git-lfs.github.com/spec/v1
+oid sha256:5158a65bdb6ece89186cc04dfeff29a210cb5554953cbcbd3628a30e9759252f
+size 49
