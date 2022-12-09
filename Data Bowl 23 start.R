f22536caed02d12312ffd303dc151975f53e3282
jackhopper <76411205+jackhopper@users.noreply.github.com>
2022-12-08 20:46:59 -0600

    Adds player age to linemen_scouting data

--- Data Bowl 23 start.R
+++ Data Bowl 23 start.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b8e3a5719fd8b3089b60b024f46f840cb8afa8d7d0b2cd4e29cddde29cd8e21
-size 2525
+oid sha256:4ed42974054173f6de163cf3d760b72cd83394c319b6ad537e5ffa0221ff9336
+size 2796
