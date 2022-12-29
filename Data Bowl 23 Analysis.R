8e882ee4cadc634e19a4f0ee73ddcc11474bcd4a
jackhopper <76411205+jackhopper@users.noreply.github.com>
2022-12-29 15:04:56 -0600

    Adds play duration and nearby defenders

--- Data Bowl 23 Analysis.R
+++ Data Bowl 23 Analysis.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6655f5128755fe70e20a95b2f1b94c4ea5e54c148a56df5e2a7490a15cb96644
-size 9688
+oid sha256:40407d9728791325b050380cda6ba6f951679d1f5d1da5fc678bcc573a924c40
+size 12620
